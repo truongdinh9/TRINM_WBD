7ad8eea4d17ad0350441026b5c3c6b87a734b419
minhtri3797 <minhtri3797@gmail.com>
2019-03-26 12:53:30 +0700

    frist

---
+++ NGON_NGU_LAP_TRINH_JAVA/GIAI_PHUONG_TRINH_BAC_1/src/Home.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("c: ");
+        double c = sc.nextDouble();
+
+        if (a != 0){
+            double answer = (c - b) / a;
+            System.out.println("The solution is: %f!" + answer);
+        }else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
