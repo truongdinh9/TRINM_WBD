3ffef99bb73b05052f57c81baa08d47ec9258ae8
minhtri3797 <minhtri3797@gmail.com>
2019-03-27 11:50:27 +0700

    Thuc hanh

---
+++ MANG_VA_PHUONG_THUC/TIM_VI_TRI_TRONG_MANG/src/main/java/Home.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+        String[] students = {"Hung", "Le", "Luyen", "Huyen", "Trang", "Hien"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name's student: ");
+        String input_name = sc.nextLine();
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Position of the student in the list " + input_name +
+                        " is: " + (i + 1));
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println("Not found" + input_name + " in the list.");
+        }
+    }
+}
