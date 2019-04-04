b6eb54a6d1a5ed59272e0c8ae90de968090a07f2
minhtri3797 <minhtri3797@gmail.com>
2019-04-04 18:04:26 +0700

    clean code

---
+++ CLEAN_CODE/java-cleancode-refactoring-exercise-master/src/TennisGame.java
@@ -0,0 +1,61 @@
+public class TennisGame {
+
+    public static String getScore(String playerOneScore, String playerTwoScore, int scoreOne, int scoreTwo) {
+        String score = "";
+        int tempScore=0;
+        if (scoreOne==scoreTwo)
+        {
+            switch (scoreOne)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+        }
+        else if (scoreOne>=4 || scoreTwo>=4)
+        {
+            int minusResult = scoreOne-scoreTwo;
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
+            else score ="Win for player2";
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = scoreOne;
+                else { score+="-"; tempScore = scoreTwo;}
+                switch(tempScore)
+                {
+                    case 0:
+                        score+="Love";
+                        break;
+                    case 1:
+                        score+="Fifteen";
+                        break;
+                    case 2:
+                        score+="Thirty";
+                        break;
+                    case 3:
+                        score+="Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
+}
---
+++ CLEAN_CODE/java-cleancode-refactoring-exercise-master/test/TennisGameTest.java
@@ -0,0 +1,81 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class TennisGameTest {
+
+    private int playerOneScore;
+    private int playerTwoScore;
+    private String expectedScore;
+
+    public TennisGameTest(int playerOneScore, int playerTwoScore, String expectedScore) {
+        this.playerOneScore = playerOneScore;
+        this.playerTwoScore = playerTwoScore;
+        this.expectedScore = expectedScore;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getAllScores() {
+        return Arrays.asList(new Object[][] {
+                { 0, 0, "Love-All" },
+                { 1, 1, "Fifteen-All" },
+                { 2, 2, "Thirty-All"},
+                { 3, 3, "Forty-All"},
+                { 4, 4, "Deuce"},
+
+                { 1, 0, "Fifteen-Love"},
+                { 0, 1, "Love-Fifteen"},
+                { 2, 0, "Thirty-Love"},
+                { 0, 2, "Love-Thirty"},
+                { 3, 0, "Forty-Love"},
+                { 0, 3, "Love-Forty"},
+                { 4, 0, "Win for player1"},
+                { 0, 4, "Win for player2"},
+
+                { 2, 1, "Thirty-Fifteen"},
+                { 1, 2, "Fifteen-Thirty"},
+                { 3, 1, "Forty-Fifteen"},
+                { 1, 3, "Fifteen-Forty"},
+                { 4, 1, "Win for player1"},
+                { 1, 4, "Win for player2"},
+
+                { 3, 2, "Forty-Thirty"},
+                { 2, 3, "Thirty-Forty"},
+                { 4, 2, "Win for player1"},
+                { 2, 4, "Win for player2"},
+
+                { 4, 3, "Advantage player1"},
+                { 3, 4, "Advantage player2"},
+                { 5, 4, "Advantage player1"},
+                { 4, 5, "Advantage player2"},
+                { 15, 14, "Advantage player1"},
+                { 14, 15, "Advantage player2"},
+
+                { 6, 4, "Win for player1"},
+                { 4, 6, "Win for player2"},
+                { 16, 14, "Win for player1"},
+                { 14, 16, "Win for player2"},
+        });
+    }
+
+    @Test
+    public void checkAllScores() {
+        int highestScore = Math.max(this.playerOneScore, this.playerTwoScore);
+        int scoreOne = 0;
+        int scoreTwo = 0;
+        for (int i = 0; i < highestScore; i++) {
+            if (i < this.playerOneScore)
+                scoreOne += 1;
+            if (i < this.playerTwoScore)
+                scoreTwo += 1;
+        }
+        assertEquals(this.expectedScore, TennisGame.getScore("John", "Bill", scoreOne, scoreTwo));
+    }
+}
