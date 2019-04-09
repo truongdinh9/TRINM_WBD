b3f7153e4e17422d7ac6a6a6e7652930bbfbf619
minhtri3797 <minhtri3797@gmail.com>
2019-04-09 15:28:37 +0700

    quan ly thu vien

--- THAO_TAC_VOI_CSDL/QUAN_LY_THU_VIEN.sql
+++ THAO_TAC_VOI_CSDL/QUAN_LY_THU_VIEN.sql
@@ -1,151 +1,36 @@
-create schema if not exists classicmodels collate latin1_swedish_ci;
+CREATE  DATABASE  LIBARY;
+USE LIBARY;
 
-create table if not exists offices
-(
-	officeCode varchar(10) not null
-		primary key,
-	city varchar(50) not null,
-	phone varchar(50) not null,
-	addressLine1 varchar(50) not null,
-	addressLine2 varchar(50) null,
-	state varchar(50) null,
-	country varchar(50) not null,
-	postalCode varchar(15) not null,
-	territory varchar(10) not null
+
+CREATE TABLE Category(
+    CATEGORY_ID              INT AUTO_INCREMENT,
+    CONSTRAINT PK_Category PRIMARY KEY (CATEGORY_ID),
+    CATEGORY_NAME            VARCHAR(50) NOT NULL
 );
 
-create table if not exists employees
-(
-	employeeNumber int not null
-		primary key,
-	lastName varchar(50) not null,
-	firstName varchar(50) not null,
-	extension varchar(10) not null,
-	email varchar(100) not null,
-	officeCode varchar(10) not null,
-	reportsTo int null,
-	jobTitle varchar(50) not null,
-	constraint employees_ibfk_1
-		foreign key (reportsTo) references employees (employeeNumber),
-	constraint employees_ibfk_2
-		foreign key (officeCode) references offices (officeCode)
+CREATE TABLE Student(
+    STUDENT_ID               INT AUTO_INCREMENT,
+    CONSTRAINT PK_Student PRIMARY KEY (STUDENT_ID),
+    STUDENT_NAME             VARCHAR(50) NOT NULL ,
+    BIRTHDAY                 DATE NOT NULL ,
+    ADDRESS                  VARCHAR(200) NOT NULL ,
+    EMAIL                    VARCHAR(100) NOT NULL ,
+    PHONE                    VARCHAR(10) NOT NULL ,
+    INMAGE                   TEXT
 );
 
-create table if not exists customers
-(
-	customerNumber int not null
-		primary key,
-	customerName varchar(50) not null,
-	contactLastName varchar(50) not null,
-	contactFirstName varchar(50) not null,
-	phone varchar(50) not null,
-	addressLine1 varchar(50) not null,
-	addressLine2 varchar(50) null,
-	city varchar(50) not null,
-	state varchar(50) null,
-	postalCode varchar(15) null,
-	country varchar(50) not null,
-	salesRepEmployeeNumber int null,
-	creditLimit decimal(10,2) null,
-	constraint customers_ibfk_1
-		foreign key (salesRepEmployeeNumber) references employees (employeeNumber)
+CREATE TABLE Book(
+    BOOK_ID                  INT AUTO_INCREMENT,
+    CONSTRAINT  PK_Book PRIMARY KEY (BOOK_ID),
+    BOOK_NAME                VARCHAR(50) NOT NULL ,
+    PUBLISHER                VARCHAR(50) NOT NULL ,
+    AUTHOR                   VARCHAR(50) NOT NULL ,
+    PUBLISHING_YEAR          DATE NOT NULL ,
+    PUBLICATION_NUMBER       INT NOT NULL ,
+    PRICE                    NUMERIC(11,2) NOT NULL ,
+    INMAGE                   TEXT,
+    CATEGORY_ID              INT NOT NULL ,
+    STUDENT_ID               INT NOT NULL ,
+    CONSTRAINT FK_Category_Book FOREIGN KEY (CATEGORY_ID) REFERENCES Category(CATEGORY_ID),
+    CONSTRAINT FK_Student_Book FOREIGN KEY (STUDENT_ID) REFERENCES Student(STUDENT_ID)
 );
-
-create index salesRepEmployeeNumber
-	on customers (salesRepEmployeeNumber);
-
-create index officeCode
-	on employees (officeCode);
-
-create index reportsTo
-	on employees (reportsTo);
-
-create table if not exists orders
-(
-	orderNumber int not null
-		primary key,
-	orderDate date not null,
-	requiredDate date not null,
-	shippedDate date null,
-	status varchar(15) not null,
-	comments text null,
-	customerNumber int not null,
-	constraint orders_ibfk_1
-		foreign key (customerNumber) references customers (customerNumber)
-);
-
-create index customerNumber
-	on orders (customerNumber);
-
-create table if not exists payments
-(
-	customerNumber int not null,
-	checkNumber varchar(50) not null,
-	paymentDate date not null,
-	amount decimal(10,2) not null,
-	primary key (customerNumber, checkNumber),
-	constraint payments_ibfk_1
-		foreign key (customerNumber) references customers (customerNumber)
-);
-
-create table if not exists productlines
-(
-	productLine varchar(50) not null
-		primary key,
-	textDescription varchar(4000) null,
-	htmlDescription mediumtext null,
-	image mediumblob null
-);
-
-create table if not exists products
-(
-	productCode varchar(15) not null
-		primary key,
-	productName varchar(70) not null,
-	productLine varchar(50) not null,
-	productScale varchar(10) not null,
-	productVendor varchar(50) not null,
-	productDescription text not null,
-	quantityInStock smallint(6) not null,
-	buyPrice decimal(10,2) not null,
-	MSRP decimal(10,2) not null,
-	constraint products_ibfk_1
-		foreign key (productLine) references productlines (productLine)
-);
-
-create table if not exists orderdetails
-(
-	orderNumber int not null,
-	productCode varchar(15) not null,
-	quantityOrdered int not null,
-	priceEach decimal(10,2) not null,
-	orderLineNumber smallint(6) not null,
-	primary key (orderNumber, productCode),
-	constraint orderdetails_ibfk_1
-		foreign key (orderNumber) references orders (orderNumber),
-	constraint orderdetails_ibfk_2
-		foreign key (productCode) references products (productCode)
-);
-
-create index productCode
-	on orderdetails (productCode);
-
-create index productLine
-	on products (productLine);
-
--- GROUP BY / COUNT
-SELECT o.status, COUNT(*) AS 'So luong status' FROM orders o GROUP BY o.status;
-
--- GROUP By / SUM
-SELECT o.status, SUM(od.priceEach * od.quantityOrdered)  AS 'TOTAL'
-FROM classicmodels.orders o JOIN classicmodels.orderdetails od ON o.orderNumber = od.orderNumber GROUP BY status DESC ;
-
---
-
-SELECT od.orderNumber ,SUM(od.quantityOrdered * od.priceEach)  AS 'total'FROM orderdetails od group by od.orderNumber;
-
--- GROUP BY / HAVING
-
-SELECT YEAR(o.orderDate) AS YEAR, SUM(od.quantityOrdered* od.priceEach) AS 'TOTAL'
-FROM classicmodels.orders o JOIN classicmodels.orderdetails od WHERE o.status='Shipped'
-GROUP BY YEAR HAVING YEAR > 2003
