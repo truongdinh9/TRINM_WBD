b61fa767afcb24a5da55f409a2e889abc5d8c65e
minhtri3797 <minhtri3797@gmail.com>
2019-05-02 08:21:45 +0700

    session & cookie

---
+++ Session&Cookie/SaveUser/src/main/java/vn/com/controller/LoginController.java
@@ -0,0 +1,63 @@
+package vn.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import vn.com.model.User;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@SessionAttributes("user")
+public class LoginController {
+
+    @ModelAttribute("user")
+    public User setUpUserForm() {
+        return new User();
+    }
+
+    @GetMapping("/login")
+    public String Index(@CookieValue(value = "setUser", defaultValue = "") String setUser, Model model) {
+        Cookie cookie = new Cookie("setUser", setUser);
+        model.addAttribute("cookieValue", cookie);
+        return "login";
+    }
+
+    @PostMapping("/doLogin")
+    public String doLogin(@ModelAttribute("user") User user,
+                          Model model,
+                          @CookieValue(value = "setUser", defaultValue = "") String setUser,
+                          HttpServletResponse response,
+                          HttpServletRequest request) {
+        if (user.getEmail().equals("admin@gmail.com") && user.getPassword().equals("12345")) {
+            if (user.getEmail() != null)
+                setUser = user.getEmail();
+
+            Cookie cookie = new Cookie("setUser", setUser);
+            cookie.setMaxAge(24 * 60 * 60);
+            response.addCookie(cookie);
+
+            Cookie[] cookies = request.getCookies();
+
+            for (Cookie ck : cookies) {
+                if (ck.getName().equals("setUser")) {
+                    model.addAttribute("cookieValue", ck);
+                    break;
+                } else {
+                    ck.setValue("");
+                    model.addAttribute("cookieValue", ck);
+                    break;
+                }
+            }
+            model.addAttribute("message", "Login success. Welcome");
+        } else {
+            user.setEmail("");
+            Cookie cookie = new Cookie("setUser", setUser);
+            model.addAttribute("cookieValue", cookie);
+            model.addAttribute("message", "login failed . Try again");
+        }
+        return "login";
+    }
+}
