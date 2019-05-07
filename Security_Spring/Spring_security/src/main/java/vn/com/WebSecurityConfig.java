d5a18797952cf4fbbe5e26fc03bf3f53b6da94b7
minhtri3797 <minhtri3797@gmail.com>
2019-05-08 01:01:03 +0700

    spring security

---
+++ Security_Spring/Spring_security/src/main/java/vn/com/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package vn.com;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("12345").roles("ADMIN");
+    }
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
---
+++ Security_Spring/Vai_tro_phan_quyen/src/main/java/vn/com/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package vn.com;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
